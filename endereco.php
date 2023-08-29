c1e1e76407dd664aad3ecc01fcbda83d5be8c8ee
Allan_Vinicius <allan.vralmeida@senacsp.edu.br>
2023-08-29 13:54:09 -0300

    29/08

--- endereco.php
+++ endereco.php
@@ -1,45 +1,23 @@
-<?php
-    if(isset($_POST['cep']) && !empty($_POST['cep']))
-    {
-        $cep= $_POST['cep'];
-        $resultado = file_get_contents("https://viacep.com.br/ws/$cep/json/");
+<?php 
 
-        $dadosEndereco = json_decode($resultado, true);
+class Endereco{
 
-        var_dump($dadosEndereco);
-    }else{
-        header("location:buscar_endereco.php");
-        exit;
+    public $cep;
+    public $logradouro;
+    public $bairro;
+    public $localidade;
+    public $uf;
+
+    public function buscarEndereco(){
+
+
+        
     }
-?>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Buscar Endereço</title>
-        <style>
-            .bloquin{
-                text-align: center;
-                margin: auto;
-                width: 320px;
-                border: 3px solid green;
-                padding: 10px;
-                color:black;
-                
-            }
-        </style>
 
-    </head>
+    public function imprimirEndereco(){
+            
+    }
 
-    <body>
-        </body>
-        <div class="bloquin">
-            <h1>Dados</h1>
-            <hr>
-            <p>CEP: <?=$dadosEndereco['cep']?></P>
-            <p>Lougradouro: <?=$dadosEndereco['logradouro'] ?></p>
-            <p>Bairro: <?=$dadosEndereco['bairro'] ?></p>
-            <p>Cidade: <?=$dadosEndereco['localidade'] ?></p>
-            <p>Estado: <?=$dadosEndereco['uf'] ?></p>
-        </div>
-        </body>
-    </html>
+
+
+}
