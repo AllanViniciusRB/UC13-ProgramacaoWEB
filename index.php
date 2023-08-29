7091d1d4503669956da20899e03862feb264e4d7
Allan_Vinicius <allan.vralmeida@senacsp.edu.br>
2023-08-29 17:18:20 -0300

    Colunas

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="container text-bg-primary pb-1 mt-2 text-center mb-2" >
+            <h1>Aula de Bootstrap</h1>
+        </div>
+
+
+        <div class="row text-center mb-2">
+
+            <div class="text-bg-dark col-12 p-3 mb-2">
+                <h2>Bootstrap</h2>
+            </div>
+            <div class="text-bg-danger p-3 col-12 col-md-4 ">
+                <h2>Coluna 1</h2>
+            </div>
+
+            <div class="text-bg-info p-3 col-6 col-md-4 ">
+                <h2>Coluna 2</h2>
+            </div>
+
+            <div class="text-bg-warning p-3 col-6 col-md-4 ">
+                <h2>Coluna 3</h2>
+            </div>
+        </div>
+
+        <div class="row text-center">
+            <div class="text-center text-bg-dark p-3 col-12 mb-2">
+                <h2>Coluna 4</h2>
+            </div>
+            <div class="text-bg-secondary p-3 col-md-6 col-md-2 mb-2">
+                <h2>Coluna 5</h2>
+            </div>
+            <div class="text-bg-warning p-3 col-md-6 col-md-2 d-none d-md-block mb-2">
+                <h2>Coluna 6</h2>
+            </div>
+            <div class="text-bg-danger p-3 col-md-12 col-md-3 ">
+                <h2>Coluna 7</h2>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+</body>
+</html>
